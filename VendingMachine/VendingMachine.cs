76d2a5636307b2c204c83a43f8491d24092d20e9
Gabriel Ortiz <gabriel.ortiz@alaskaair.com>
2017-04-05 17:05:04 -0700

    Refactored to use nickel calculation

--- VendingMachine/VendingMachine.cs
+++ VendingMachine/VendingMachine.cs
@@ -18,43 +18,52 @@ namespace VendingMachine
             int nickelsProvided = 0;
             int dimesProvided = 0;
 
-            if (amountOwed >= .15M)
+            // find the nickels needed to make change
+            int nickelsNeeded = (int) (amountOwed / .05M);
+            if (nickels >= nickelsNeeded)
             {
-                if (dimes > 1)
-                {
-                    dimesProvided = 1;
-                    nickelsProvided = 1;
-                }
-                else
-                {
-                    nickelsProvided = 3;
-                }
+                nickelsProvided = nickelsNeeded;
             }
-
-            if (amountOwed >= .10M)
+            else
             {
-                if (dimes == 1)
+                if (amountOwed >= .15M)
                 {
-                    dimesProvided = 1;
+                    if (dimes > 1)
+                    {
+                        dimesProvided = 1;
+                        nickelsProvided = 1;
+                    }
+                    else
+                    {
+                        nickelsProvided = 3;
+                    }
                 }
-                else if (nickels == 2)
-                {
-                    nickelsProvided = 2;
-                }
-            }
 
-            if (amountOwed == .05M)
-            {
-                if (nickels == 1)
+                if (amountOwed >= .10M)
                 {
-                    nickelsProvided = 1;
+                    if (dimes == 1)
+                    {
+                        dimesProvided = 1;
+                    }
+                    else if (nickels == 2)
+                    {
+                        nickelsProvided = 2;
+                    }
+                }
+
+                if (amountOwed == .05M)
+                {
+                    if (nickels == 1)
+                    {
+                        nickelsProvided = 1;
+                    }
                 }
             }
 
             if (nickelsProvided > 0 || dimesProvided > 0)
             {
                 return
-                    $"Change is {nickelsProvided} Nickel(s), {dimesProvided} Dime(s), 0 quarter(s).";
+                    $"Change is {nickelsProvided} nickel(s), {dimesProvided} dime(s), 0 quarter(s).";
             }
             return "Unable to give change.";
         }
