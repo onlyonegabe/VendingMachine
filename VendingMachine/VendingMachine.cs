c5cd85cdf4d6c470793173481b97d469a25c4ae0
Gabriel Ortiz <gabriel.ortiz@alaskaair.com>
2017-04-09 13:31:01 -0700

    Going a different direction, removed tests for clearity, added dime test, and refactored

--- VendingMachine/VendingMachine.cs
+++ VendingMachine/VendingMachine.cs
@@ -4,52 +4,52 @@ namespace VendingMachine
     {
         private int nickels;
         private int dimes;
-        private int quarters;
+        private const decimal NickelValue = .05M;
+        private const decimal DimeValue = .10M;
 
         public VendingMachine(int nickels, int dimes, int quarters)
         {
             this.nickels = nickels;
             this.dimes = dimes;
-            this.quarters = quarters;
         }
 
         public string GiveChange(decimal amountOwed)
         {
-            int nickelsProvided = 0;
-            int dimesProvided = 0;
+            int nickelsInChange = 0;
+            int dimesInChange = 0;
 
-            // find the nickels needed to make change
-            int nickelsNeeded = (int) (amountOwed / .05M);
-            if (nickels >= nickelsNeeded)
-            {
-                nickelsProvided = nickelsNeeded;
-            }
-            else
-            {
-                if (amountOwed >= .15M)
-                {
-                    if (dimes > 1)
-                    {
-                        dimesProvided = 1;
-                        nickelsProvided = 1;
-                    }
-                }
+            dimesInChange = MakeChangeForCoinType(amountOwed, DimeValue);
+            nickelsInChange = MakeChangeForCoinType(amountOwed, NickelValue);
 
-                if (amountOwed >= .10M)
-                {
-                    if (dimes == 1)
-                    {
-                        dimesProvided = 1;
-                    }
-                }
-            }
-
-            if (nickelsProvided > 0 || dimesProvided > 0)
+            if (nickelsInChange > 0 || dimesInChange > 0)
             {
                 return
-                    $"Change is {nickelsProvided} nickel(s), {dimesProvided} dime(s), 0 quarter(s).";
+                    $"Change is {nickelsInChange} nickel(s), {dimesInChange} dime(s), 0 quarter(s).";
             }
             return "Unable to give change.";
+        }
+
+        private int MakeChangeForCoinType(decimal amountOwed, decimal coinValue)
+        {
+            int coinsNeeded = (int)(amountOwed / coinValue);
+            int coinsProvided = 0;
+
+            if (coinValue == DimeValue)
+            {
+                if (dimes >= coinsNeeded)
+                {
+                    coinsProvided = coinsNeeded;
+                }
+            }
+            if (coinValue == NickelValue)
+            {
+                if (nickels >= coinsNeeded)
+                {
+                    coinsProvided = coinsNeeded;
+                }
+            }
+
+            return coinsProvided;
         }
     }
 }
--- VendingMachine/VendingMachineTests.cs
+++ VendingMachine/VendingMachineTests.cs
@@ -4,14 +4,9 @@ namespace VendingMachine
 {
     public class VendingMachineTests
     {
-        [TestCase(0.65, 13, 0, 0, "Change is 13 nickel(s), 0 dime(s), 0 quarter(s).")]
-        [TestCase(0.00, 0, 0, 0, "Unable to give change.")]
-        [TestCase(0.05, 1, 0, 0, "Change is 1 nickel(s), 0 dime(s), 0 quarter(s).")]
+        [TestCase(0.70, 0, 7, 0, "Change is 0 nickel(s), 7 dime(s), 0 quarter(s).", Description = "Give dimes")]
+        [TestCase(0.65, 13, 0, 0, "Change is 13 nickel(s), 0 dime(s), 0 quarter(s).", Description = "Give nickels")]
         [TestCase(0.05, 0, 0, 0, "Unable to give change.", Description = "5 cents owed but no change available.")]
-        [TestCase(0.10, 0, 1, 0, "Change is 0 nickel(s), 1 dime(s), 0 quarter(s).")]
-        [TestCase(0.10, 2, 0, 0, "Change is 2 nickel(s), 0 dime(s), 0 quarter(s).")]
-        [TestCase(0.15, 3, 0, 0, "Change is 3 nickel(s), 0 dime(s), 0 quarter(s).")]
-        [TestCase(0.15, 1, 2, 0, "Change is 1 nickel(s), 1 dime(s), 0 quarter(s).")]
         public void GiveChange_ProvidedInventory_ReturnsExpected(decimal amountOwed, int nickels, int dimes, int quarters, string expected)
         {
             // Arrange
