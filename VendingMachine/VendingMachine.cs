c3bd0ac0af53ac6be3c6e5ca8796e356463073c6
Gabriel Ortiz <gabriel.ortiz@alaskaair.com>
2017-04-17 12:59:04 -0700

    Refactoing by extracting methods to calculate the kind of change.

--- VendingMachine/VendingMachine.cs
+++ VendingMachine/VendingMachine.cs
@@ -3,41 +3,51 @@ namespace VendingMachine
     public class VendingMachine
     {
         private int nickels;
-        private int dimes;
         private int quarters;
 
         public VendingMachine(int nickels, int dimes, int quarters)
         {
             this.nickels = nickels;
-            this.dimes = dimes;
             this.quarters = quarters;
         }
 
         public string GiveChange(decimal amountOwed)
         {
-            int nickelsToGiveCustomer = 0;
             int dimesProvided = 0;
-            int quartersToGiveCustomer = 0;
 
-            const decimal quarterValue = .25M;
-            int quartersNeeded = GetCoinsNeededToMakeChange(amountOwed, quarterValue);
-            if (AreThereEnoughCoins(quarters, quartersNeeded))
-            {
-                quartersToGiveCustomer = quartersNeeded;
-            }
-            const decimal nickelValue = .05M;
-            int nickelsNeeded = GetCoinsNeededToMakeChange(amountOwed, nickelValue);
-            if (AreThereEnoughCoins(nickels, nickelsNeeded))
-            {
-                nickelsToGiveCustomer = nickelsNeeded;
-            }
-            
+            int quartersToGiveCustomer = CalculateQuartersInChangeToGiveCustomer(amountOwed);
+            int nickelsToGiveCustomer = CalculateNickelsInChangeToGiveCustomer(amountOwed);
+
             if (nickelsToGiveCustomer > 0 || dimesProvided > 0 || quartersToGiveCustomer > 0)
             {
                 return
                     $"Change is {nickelsToGiveCustomer} nickel(s), {dimesProvided} dime(s), {quartersToGiveCustomer} quarter(s).";
             }
             return "Unable to give change.";
+        }
+
+        private int CalculateNickelsInChangeToGiveCustomer(decimal amountOwed)
+        {
+            int nickelsToGiveCustomer = 0;
+            const decimal nickelValue = .05M;
+            int nickelsNeeded = GetCoinsNeededToMakeChange(amountOwed, nickelValue);
+            if (AreThereEnoughCoins(nickels, nickelsNeeded))
+            {
+                nickelsToGiveCustomer = nickelsNeeded;
+            }
+            return nickelsToGiveCustomer;
+        }
+
+        private int CalculateQuartersInChangeToGiveCustomer(decimal amountOwed)
+        {
+            int quartersToGiveCustomer = 0;
+            const decimal quarterValue = .25M;
+            int quartersNeeded = GetCoinsNeededToMakeChange(amountOwed, quarterValue);
+            if (AreThereEnoughCoins(quarters, quartersNeeded))
+            {
+                quartersToGiveCustomer = quartersNeeded;
+            }
+            return quartersToGiveCustomer;
         }
 
         private bool AreThereEnoughCoins(int coinsInInventory, int coinsNeeded)
